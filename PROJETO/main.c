27fc328b059fc657d9e16dcdb0b4c81e5c64e29d
victorjonhson <vjonhson498@gmail.com>
2024-08-16 13:54:49 -0300

    Testando metodo de criar tabela

--- PROJETO/main.c
+++ PROJETO/main.c
@@ -5,7 +5,8 @@
 
 // aqui estamos criando um tipo aluno (que sera uma tabela)
 
-struct aluno {
+struct aluno { 
+    // a matricula será a primary key
     int matricula;
     char nome[100];
     int idade;
@@ -18,20 +19,55 @@ int main(int argc, char argv[]) {
 
     setlocale(LC_ALL, "Portuguese");
 
-    aluno alunos[10][4];
+    // criando uma tabela do tipo aluno
+    aluno alunos[1][4];
+    int lin;
+    int col;
 
-    for (int lin = 1; lin < 10; lin++) {
-        for (int col = 1; col < 4; col++) {
+    //int contLinha = 1;
+
+    // percorrendo linha da tabela e adicionando dados nas colunas
+    for (lin = 1; lin <= 1; lin++) {
+        for (int col = 1; col <= 4; col++) {
             if (col == 1) {
+                puts("Matricula (chave primária):");
+                scanf("%d", &alunos[lin][col].matricula);
+
+            } else if (col == 2) {
                 puts("Nome:");
-                scanf("%100[^\n]s", &alunos[lin][col].nome);
+                scanf("%s", &alunos[lin][col].nome);
+
+            } else if (col == 3) {
+                puts("Idade:");
+                scanf("%d", &alunos[lin][col].idade);
+
+            } else if (col == 4) {
+                puts("Indice Academico:");
+                scanf("%f", &alunos[lin][col].ira);
             } 
         }
     }
 
+    // immprimindo tabela
+    for (lin = 1; lin <= 1; lin++) {
+        printf("------------------Pessoa %d ----------------- \n", lin);
+        
+        for (col = 1; col <= 4; col++) {
+            if (col == 1) {
+                printf("Matricula: %d", alunos[lin][col].matricula);
+            } else if (col == 2) {
+                printf("Nome: %s \n", alunos[lin][col].nome);
 
+            } else if (col == 3) {
+                printf("Idade: %d \n", alunos[lin][col].idade);
 
+            } else if (col == 4) {
+                printf("Indice academico: %.2f \n", alunos[lin][col].ira);
+            } 
+        }
 
+        printf("--------------------------------------------- \n");
+    }
 
     return 0;
 }
